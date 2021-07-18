7206b7f4270dad76c95ac342ab2b639178fd8687
carneyaj <50503055+carneyaj@users.noreply.github.com>
2021-07-18 13:11:05 -0400

    Create params.py

---
+++ params.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+
+Call analysis functions on an existing .wav file
+Resample to 16kHz
+
+'''
+
+samplerate = 16000	# Audio samplerate
+seconds = 20		# length of each window of analysis
+blocks = 1		# number of analysis blocks of length *seconds* 
+gain = 20   		# multiplier on audio samples
+
+save_directory = "/home/pi/data/"
+out_dtype = "float32"
+
+top_classes = 5
+
+n_fft = 512
+hop_length = 256
+mels = 40
+fmin = 2000
+fmax = 8000
+
+interval = 10
